befad17a27b776f24d2ea0bf3da89cf5bd1fd182
molarmanful <benjinpang@gmail.com>
2024-02-09 04:00:55 -0500

    404 page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,11 +3,15 @@
   import { Nav } from '$lib'
 </script>
 
+<svelte:head>
+  <title>{$page.error.message} | Aerolympics</title>
+</svelte:head>
+
 <Nav />
 
 <div class="screen flex flex-justify-center flex-items-center">
   <div class="text-center">
-    <h1 class="text-b text-[4rem]">{$page.status}</h1>
+    <h1 class="text-[4rem] text-b">{$page.status}</h1>
     <h2>{$page.error.message}</h2>
   </div>
 </div>
